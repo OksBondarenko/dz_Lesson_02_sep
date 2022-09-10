9dacafeb663e0e04af5e5784123bb9f7604a3a04
Oksana bondarenko <roksanapopova@gmail.com>
2022-09-10 23:48:48 +0200

    task 2 Показать натуральные числа от M до N

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Показать натуральные числа от M до N, N и M заданы
+
+void Number(int m, int n)
+{
+    if (n >= m)
+    {
+        Console.Write(m + " "); 
+        Number(m + 1, n);
+    }
+}
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Number(m, n);
