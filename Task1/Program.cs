19331180b414e3b1bd03779a8b405e58078dc704
Oksana bondarenko <roksanapopova@gmail.com>
2022-09-10 23:09:46 +0200

    Task 1 N...1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Показать натуральные числа от N до 1, N задано
+
+void Number(int num)
+{
+    if (num >= 1) 
+    {
+        Console.Write(num + " ");
+        Number(num - 1);
+    }
+}
+
+Console.Write("Введите число: ");
+Number(Convert.ToInt32(Console.ReadLine()));
+
